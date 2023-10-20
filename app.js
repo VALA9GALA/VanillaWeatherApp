2825c820ff32d8526a9a40080a7815e2bba6386e
VALA9GALA <147095924+VALA9GALA@users.noreply.github.com>
2023-10-19 22:24:56 -0400

    Added API call data to the forecast

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatForecastDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "045ace03oteb7d0da03b1286fde00d59";
@@ -63,27 +70,38 @@ function displayTemperature(response) {
 }
 function displayForecast(response) {
   console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Wed", "Thu", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
-                <div class="forecast-date">${day}</div>
+  forecast.slice(1).forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+                <div class="forecast-date">${formatForecastDay(
+                  forecastDay.time
+                )}</div>
+            
                 <img
-                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+                    forecastDay.condition.icon
+                  }.png"
                   alt=""
                   width="42"
                 />
                 <div class="forecast-temp">
-                  <span class="forecast-temp-max">18°/</span>
-                  <span class="forecast-temp-min">12°</span>
+                  <span class="forecast-temp-max">${Math.round(
+                    forecastDay.temperature.maximum
+                  )}°/</span>
+                  <span class="forecast-temp-min">${Math.round(
+                    forecastDay.temperature.minimum
+                  )}°</span>
                 </div>
               </div>
               
         `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
